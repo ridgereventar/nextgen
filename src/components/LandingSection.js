e5d9e7ed0ea94cbab60ec95e07c9aa74eac4f643
ridgereventar <ridge.reventar@gmail.com>
2021-03-16 02:22:59 -0400

    troubleshooting landing

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -14,7 +14,7 @@ const LandingSection = (props) => {
         <div className="section landing-section">
             
             <div className="outer-row">
-                {props.logo?
+                {/* {props.logo?
                     <img id="mobile-logo" src={logo} alt=""></img> 
                     :null
                 }
@@ -24,22 +24,22 @@ const LandingSection = (props) => {
                         <h1>Foundations</h1>
                     </div>  
                     :null  
-                }
+                } */}
             </div>
             
             <div className="manual-container">
-                <Fade delay={500}>
+                {/* <Fade delay={500}>
                     <div className="manual-wrapper">
                         <img id="manual" src={props.manual} alt=""></img>
                         <img id="gold" src={gold} alt=""></img>
                     </div>
-                </Fade>
+                </Fade> */}
             </div>
             
             <div className="outer-row">
-                <Fade delay={1500}>
+                {/* <Fade delay={1500}>
                     <img id="arrow" src={arrow} alt=""></img>
-                </Fade>                    
+                </Fade>                     */}
             </div>
 
         </div> 
