cb479264b762f5d443122f0eba68ed72eb93f950
ridgereventar <ridge.reventar@gmail.com>
2021-03-19 09:45:52 -0400

    compressed landing manual

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -7,7 +7,7 @@ import Nav from '../components/Nav';
 import LandingSection from '../components/LandingSection';
 import Workshops from '../components/Workshops';
 
-import introManual from '../images/landingManual.png';
+import introManual from '../images/landingManualV2.png';
 import video from '../images/nextgenvid.mp4';
 import connect from '../images/bg/connect2.jpg';
 import thumbnail from '../images/poster.png';
